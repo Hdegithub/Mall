698d6f827ce92053f6ef95cd850e2eef8f95f13b
hejianbo <2948426785@qq.com>
2023-10-23 16:26:44 +0800

    后台分页查询订单列表和根据id查询订单详情

---
+++ mall/src/main/java/com/geekaca/mall/controller/admin/OrderController.java
@@ -0,0 +1,57 @@
+package com.geekaca.mall.controller.admin;
+
+import com.geekaca.mall.controller.vo.OrderDetailVO;
+import com.geekaca.mall.service.OrderService;
+import com.geekaca.mall.utils.PageQueryUtil;
+import com.geekaca.mall.utils.Result;
+import com.geekaca.mall.utils.ResultGenerator;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Api(value = "v1", tags = "8-5.后台管理系统订单模块接口")
+@RequestMapping("/manage-api/v1")
+@Slf4j
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    /**
+     * 获取订单列表
+     */
+    @RequestMapping(value = "/orders", method = RequestMethod.GET)
+    @ApiOperation(value = "订单列表", notes = "可根据订单号和订单状态筛选")
+    public Result list(@RequestParam(required = false) @ApiParam(value = "页码") Integer pageNumber,
+                       @RequestParam(required = false) @ApiParam(value = "每页条数") Integer pageSize,
+                       @RequestParam(required = false) @ApiParam(value = "订单号") String orderNo,
+                       @RequestParam(required = false) @ApiParam(value = "订单状态") Integer orderStatus) {
+        if (pageNumber == null || pageNumber < 1 || pageSize == null || pageSize < 10) {
+            return ResultGenerator.genFailResult("分页参数异常！");
+        }
+        Map params = new HashMap(8);
+        params.put("page", pageNumber);
+        params.put("limit", pageSize);
+        if (StringUtils.hasText(orderNo)) {
+            params.put("orderNo", orderNo);
+        }
+        if (orderStatus != null) {
+            params.put("orderStatus", orderStatus);
+        }
+        PageQueryUtil pageUtil = new PageQueryUtil(params);
+        return ResultGenerator.genSuccessResult(orderService.getOrdersPage(pageUtil));
+    }
+
+    @GetMapping("/orders/{orderId}")
+    @ApiOperation(value = "订单详情接口", notes = "传参为订单号")
+    public Result<OrderDetailVO> orderDetailPage(@ApiParam(value = "订单号") @PathVariable("orderId") Long orderId) {
+        return ResultGenerator.genSuccessResult(orderService.getOrderByOrderId(orderId));
+    }
+}
